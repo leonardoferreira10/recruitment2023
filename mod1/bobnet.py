1f373cafa44b78284d832310cf0aec7e171c6159
carlostojal <carlos.tojal@hotmail.com>
2023-10-07 20:31:50 +0100

    add a missing flattening layer

--- mod1/bobnet.py
+++ mod1/bobnet.py
@@ -7,11 +7,16 @@ class BobNet(nn.Module):
 
         super().__init__()
 
+        self.flatten = nn.Flatten()
+
         # define the two fully-connected layers
         self.fc1 = nn.Linear(784, 800)
         self.fc2 = nn.Linear(800, 10)
 
     def forward(self, x):
+
+        # flatten the input into a 1-D tensor
+        x = self.flatten(x)
 
         # do a forward pass and ReLU activation on the first layer
         x = self.fc1(x)
